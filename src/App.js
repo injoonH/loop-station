c61de23a0c3ee515360da5ce020795f05e10d0df
Injoon Hwang <injoon0617@gmail.com>
2022-07-24 18:06:21 +0900

    ✨ Create audio recorder & entry

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import Sketch from 'react-p5';
-import 'p5/lib/addons/p5.sound';
 import './App.css';
+import AudioEntry from './components/AudioEntry';
+import { AudioPlayerProvider } from 'react-use-audio-player';
+import AudioRecorder from './components/AudioRecorder';
 
 const App = () => {
-    const preload = (p5) => {};
-
-    const setup = (p5, parentRef) => {
-        p5.createCanvas(400, 400).parent(parentRef);
-    };
-
-    const draw = (p5) => {
-        p5.background(50);
-    };
-
+    const urls = [
+        'http://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/theme_01.mp3',
+        'http://commondatastorage.googleapis.com/codeskulptor-assets/jump.ogg',
+        'http://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/music/start.ogg',
+    ];
     return (
         <div>
-            <Sketch preload={preload} setup={setup} draw={draw} />
+            <AudioRecorder />
+            {urls.map((url) => (
+                <AudioPlayerProvider key={url}>
+                    <AudioEntry audioFile={url} />
+                </AudioPlayerProvider>
+            ))}
         </div>
     );
 };
