0de7613b6fdd8c2e47e0900b6b0e0c88dc1c150e
Injoon Hwang <injoon0617@gmail.com>
2022-07-24 15:34:58 +0900

    ✨ Create ToggleButton component

---
+++ src/components/button/ToggleButton.js
@@ -0,0 +1,25 @@
+import './ToggleButton.css';
+
+const ToggleButton = ({
+    state,
+    setState,
+    trueComp,
+    falseComp,
+    trueCb,
+    falseCb,
+}) => {
+    return (
+        <div
+            className="toggle-btn"
+            onClick={() => {
+                if (state === true) trueCb();
+                else falseCb();
+                setState((curr) => !curr);
+            }}
+        >
+            {state ? trueComp : falseComp}
+        </div>
+    );
+};
+
+export default ToggleButton;
